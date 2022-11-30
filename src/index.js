d4102bf600dc0fe79d75f3ebf5f08ee821845db2
Pavel Yaroslavovich <pavel.yaroslavovich22@gmail.com>
2022-11-30 10:29:36 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { HashRouter } from "react-router-dom"
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from "react-router-dom"
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<HashRouter>
-		<React.StrictMode>
+	<React.StrictMode>
+		<HashRouter>
 			<App />
-		</React.StrictMode>
-	</HashRouter>
+		</HashRouter>
+	</React.StrictMode>
 );
 
